371eaf7c02ec065eb177067440713199de84de0b
princess-par <pienaarashida@gmail.com>
2020-07-03 11:17:16 +0000

    day 10 push

---
+++ mypython.py
@@ -0,0 +1,9 @@
+in python we don't use var or let to indicat a variable is just eg;
+
+    myname = "Pienaa Shida"
+    mynumber = 100
+    mybool = True
+
+    def full_name(first, last):
+    return first + "" + last
+
